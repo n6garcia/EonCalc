f8c459519338d002abf58ab1f7a70d43bdb58905
Noel Garcia <n6garcia@ucsd.edu>
2024-07-31 05:45:05 -0700

    python bs update

--- cython.py
+++ cython.py
@@ -1,3 +1,23 @@
+#!/usr/bin/python3
+
+## LINUX ONLY
+
+## Build Flags
+#
+# --windows [default:linux] - compile target for windows
+# --gdb [default:disabled] - enables cmd debugger
+# --gen-main [default:disabled] - generates c++ from python
+#
+##
+
+import os, sys, subprocess
+
+CC = "g++"
+C = "gcc"
+
+libs = []  # ex -lGL
+
+
 def genmain():
     o = []
 
@@ -21,3 +41,61 @@ def genbplate():
     """
 
     return BPLATE
+
+
+def build():
+    cpps = []
+    obfiles = []
+
+    open("/tmp/gen.main.cpp", "wb").write(genmain().encode("utf-8"))
+    file = "/tmp/gen.main.cpp"
+    print(file)
+    ofile = "/tmp/%s.o" % file
+    obfiles.append(ofile)
+    cpps.append(file)
+    cmd = [
+        #'gcc',
+        C,
+        "-c",  ## do not call the linker
+        "-fPIC",  ## position indepenent code
+        "-o",
+        ofile,
+        os.path.join(srcdir, file),
+    ]
+    print(cmd)
+    subprocess.check_call(cmd)
+
+    os.system("ls -lh /tmp/*.o")
+
+    ## finally call the linker,
+    ## note: there's better linkers we could use here, like gold and mold
+
+    cmd = (
+        [
+            #'ld',
+            "g++",
+            "-shared",
+            "-o",
+            "/tmp/obelisk.so",
+        ]
+        + obfiles
+        + libs
+    )
+
+    print(cmd)
+    subprocess.check_call(cmd)
+
+    exe = "/tmp/obelisk"
+    cmd = [
+        C,
+        "-o",
+        exe,
+    ]
+    cmd += obfiles + libs
+
+    print(cmd)
+
+    subprocess.check_call(cmd)
+
+
+build()
